48c34d262ca0aa1f31eb087c50ad8b921369bab1
Pacey0216 <paveguteza@gmail.com>
2026-08-08 23:37:48 +0800

    Point Master Pogi config to the Products tab

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.STORE_CONFIG = {
   "brandName": "Master Pogi",
   "apiUrl": "",
-  "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=537048309#gid=537048309",
+  "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=1807963011#gid=1807963011",
   "storeFilter": "masterpogi",
   "messengerUrl": "https://m.me/YOUR_MASTER_POGI_USERNAME",
   "currency": "PHP",
   "locale": "en-PH",
-  "sampleMode": true
+  "sampleMode": false
 };
