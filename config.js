69e78b15b8143242c228fe0edb8a1cc160946e96
Pacey0216 <paveguteza@gmail.com>
2026-08-08 23:48:42 +0800

    Connect Master Pogi to live inventory API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.STORE_CONFIG = {
   "brandName": "Master Pogi",
-  "apiUrl": "",
+  "apiUrl": "https://script.google.com/macros/s/AKfycbx6ys80ltiEvWuzOCskTR7vocWm9nNvEDQPDbojwzIA3j99G74DM3Wz6OPt4y4h9A/exec",
   "sheetReference": "https://docs.google.com/spreadsheets/d/1ljK2RqkdA8E3iEpEutrhMnTurxGqXwY6Kb8yF2hW3S8/edit?gid=1807963011#gid=1807963011",
   "storeFilter": "masterpogi",
   "messengerUrl": "https://m.me/YOUR_MASTER_POGI_USERNAME",
